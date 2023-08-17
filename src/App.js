ad259dbbb61f020e4cf61bee7e574bbe94f16acd
willl85 <ngowilfried@gmail.com>
2023-08-17 13:39:52 +0200

    Calculate the investment data and send them in ResultsTable component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import ResultsTable from './components/ResultsTable/ResultsTable';
 import UserInput from './components/UserInput/UserInput';
 
 function App() {
+  const [userInput, setUserInput] = useState(null);
+
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+    // console.log(userInput);
+    setUserInput(userInput);
+  };
 
-    const yearlyData = []; // per-year results
+  // console.log(userInput);
+  const yearlyData = [];
 
-    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+  if (userInput) {
+    let currentSavings = +userInput['current-savings'];
+    const yearlyContribution = +userInput['yearly-contribution'];
     const expectedReturn = +userInput['expected-return'] / 100;
     const duration = +userInput['duration'];
 
-    // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
         yearlyContribution: yearlyContribution,
       });
     }
-
-    // do something with yearlyData ...
-  };
+  }
 
   return (
     <div>
       <Header />
 
-      <UserInput />
-
-      <ResultsTable />
+      <UserInput onCalculate={calculateHandler} />
+      {!userInput && <p>No investment yet.</p>}
+      {userInput && <ResultsTable data={yearlyData} />}
     </div>
   );
 }
